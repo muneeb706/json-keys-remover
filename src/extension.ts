cd42f6a22bff59ad8b395ec07436af577bd794b3
muneeb706 <muneeb706@gmail.com>
2023-05-19 22:04:49 -0700

    refactor(extension.tx): Refactoring and Docstrings
    following functions refactored and docstrings added for them:
    
    - getKeys
    - initJSONData
    - separateTheTags
    
    #8

--- src/extension.ts
+++ src/extension.ts
@@ -4,57 +4,54 @@ import * as vscode from 'vscode';
 
 // Functions which use later
 interface tag {
-	tag:string;
-	has:Array<string>
+	tag: string;
+	has: Array<string>
 }
+
+interface JSONData {
+  [key: string]: any;
+}
+
+interface ParsedJSON {
+  parsedJSON: JSONData;
+  keys: string[];
+}
+
 /**
- * Get keys in json data
+ * Recursively searches an object for all keys and adds them to the array 'keys'
+ * @param obj - The object to search
+ * @param pre - The string to add as a prefix to the current key
+ * @param keys - The array to hold all keys found
  */
-export function getKeys(obj:any, pre:string, keys:string[]){
+export const getKeys = (obj: any, pre: string, keys: string[]) => {
+    pre += obj.tag + ".";
+    const { val } = obj;
 
-	// console.log(obj.tag);
-	pre += obj.tag + ".";
-
-	if (Array.isArray(obj.val)) {
-		// console.log("array");
-		for (let index = 0; index < obj.val.length; index++) {
-			const element = obj.val[index];
-			getKeys({
-				tag:index,
-				val:element
-			}, pre, keys);
-		}
-	
-	}
-	//Hep objenin içindekilere ! koyunuyor.
-	//Gidebildiği kadar gidiyor.Her bölündüğünde bölünen her parça için en uca kadar gidiyor.
-	//Her ünlem koyduğunda keys a push la
-	else if(typeof obj.val === 'object'){
-		// console.log("object");
-		for (var key of Object.keys(obj.val)) {
-
-			// let addKEy = pre === '' ? `['object'][${key}]` : pre+'.'+ `['object'][${key}]`;
-			// console.log(addKEy);
-			keys.push(pre+`[${key}]`)
-			getKeys({
-				tag:`[${key}]`,
-				val:obj.val[key]
-			}, pre, keys);
-		}
-	}
-} 
+    if (Array.isArray(val)) {
+        val.forEach((element, index) => {
+            const newTag = { tag: index, val: element };
+            getKeys(newTag, pre, keys);
+        });
+    } else if (typeof val === "object") {
+        for (const [key, value] of Object.entries(val)) {
+            const newTag = { tag: `[${key}]`, val: value };
+            keys.push(`${pre}${newTag.tag}`);
+            getKeys(newTag, pre, keys);
+        }
+    }
+};
 /**
  * Delete selected keys in json data
  */
-export function deletePropByString(obj:Object, propString:string) {
-				
+export function deletePropByString(obj: Object, propString: string) {
+
 	// if(typeof obj !== "object" || Array.isArray(obj)){return;}
 
 	var prop, props = propString.split('.');
 
 	for (var i = 0, iLen = props.length - 1; i < iLen; i++) {
 		prop = props[i];
-		
+
 		var candidate = obj[prop as keyof typeof obj];
 		if (candidate !== undefined) {
 			obj = candidate;
@@ -62,46 +59,61 @@ export function deletePropByString(obj:Object, propString:string) {
 			break;
 		}
 	}
-	if(!obj.hasOwnProperty(props[i])) { return; }
+	if (!obj.hasOwnProperty(props[i])) { return; }
 	delete obj[props[i] as keyof typeof obj];
 }
 
 /**
- * Parse json data and extract keys
+ * This function takes a string containing JSON data (string) as input and 
+ * returns an object with two properties - 'parsedJSON' and 'keys'.
+ * 'parsedJSON' is the parsed JSON object obtained after parsing the input JSON string. 
+ * 'keys' is an array containing all the keys present in the input JSON object.
+ * 
+ * @param stringJSONData - A string containing JSON data.
+ * 
+ * @throws {Error} - If the input JSON string is empty.
+ * @throws {Error} - If the input JSON string has incorrect format.
+ * @throws {Error} - If no keys are found in the input JSON object.
+ * 
+ * @returns An object with two properties - 'parsedJSON' and 'keys'.
  */
-export function initJSONdata(stringJSONData:string) {
-	if(stringJSONData.trim() === ""){throw new Error("Empty JSON File");}
-	let dataJsonArray = ""
-	try {
-		dataJsonArray = JSON.parse(stringJSONData);
-	} catch (error) {
-		// console.error(error)
-		throw new Error("Incorrect JSON format");
-	}
+export function initJSONdata(stringJSONData: string): ParsedJSON {
+  if (stringJSONData.trim() === "") {
+    throw new Error("Empty JSON File");
+  }
+  let dataJsonArray: JSONData = {};
+  try {
+    dataJsonArray = JSON.parse(stringJSONData) as JSONData;
+  } catch (error) {
+    throw new Error("Incorrect JSON format");
+  }
 
-	let keys: string[] = [];
-	getKeys(
-		{
-			tag:"root",
-			val:dataJsonArray
-		}
-		,'',keys);
+  let keys: string[] = [];
+  getKeys(
+    {
+      tag: "root",
+      val: dataJsonArray,
+    },
+    "",
+    keys
+  );
 
-	if(keys.length === 0){throw new Error("No found keys");}
+  if (keys.length === 0) {
+    throw new Error("No found keys");
+  }
 
-	return {
-		parsedJSON: dataJsonArray,
-		keys: keys
-	};
-	
+  return {
+    parsedJSON: dataJsonArray,
+    keys: keys,
+  };
 }
 
 /**
  * Extract selected keys from json data
  */
-export function removeKeysinJSONData(parsedJSON:any, selectedItems: string[], tags:Array<tag>){
+export function removeKeysinJSONData(parsedJSON: any, selectedItems: string[], tags: Array<tag>) {
 
-	if(selectedItems.length === 0){throw new Error("The selected list is empty");}
+	if (selectedItems.length === 0) { throw new Error("The selected list is empty"); }
 
 	for (const key of selectedItems) {
 		tags.filter(x => x.tag === key).forEach(x => {
@@ -113,29 +125,24 @@ export function removeKeysinJSONData(parsedJSON:any, selectedItems: string[], ta
 }
 
 /**
- * Separate the tags
+ * Given an array of strings representing object keys, extracts 
+ * any tags enclosed in square brackets and groups the keys 
+ * that have the same tag.
+ * @param {string[]} keys - An array of strings representing object keys.
+ * @returns {tag[]} An array of objects, each containing a tag and an 
+ * array of keys that have that tag.
  */
-export function separateTheTags(keys:string[]){
-	let showingTag: Array<tag> = [];
-	keys.forEach(x => {
-		var matches = x.match(/\[([^\]]*)\]/g)?.join('.').replace(/[\[\]']+/g,'');
-		if(matches === undefined){return};
-		let seacrh = showingTag.findIndex(y => y.tag === matches);
-		if(seacrh === -1){
-			showingTag.push({
-				tag:matches,
-				has:[x.replace("root.",'').replace(/[\[\]']+/g,'')]
-			});
-		}
-		else if(seacrh !== -1)
-		{
-			showingTag[seacrh].has.push(x.replace("root.",'').replace(/[\[\]']+/g,''));
-		}
-	})
-	return showingTag;
+export function separateTheTags(keys: string[]) {
+  let showingTag: tag[] = [];
+  keys.forEach(key => {
+    const matches = key.match(/\[([^\]]*)\]/g)?.join('.').replace(/[\[\]']+/g, '');
+    if (!matches) return;
+    const seacrh = showingTag.findIndex(tag => tag.tag === matches);
+    if (seacrh === -1) showingTag.push({ tag: matches, has: [key.replace("root.", '').replace(/[\[\]']+/g, '')] });
+    else showingTag[seacrh].has.push(key.replace("root.", '').replace(/[\[\]']+/g, ''));
+  });
+  return showingTag;
 }
-
-
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -168,17 +175,16 @@ export async function activate(context: vscode.ExtensionContext) {
 				return; // no editor or no json file
 			}
 
-		
 			const data = initJSONdata(editor.document.getText());
 			console.log(data)
 			vscode.window.showInformationMessage('JSON file parsed');
-	
+
 			// Create option for quickPick
-			
+
 			let showingTag = separateTheTags(data.keys);
-			
+
 			const options = Array.from(showingTag, x => {
-				return({label:x.tag});
+				return ({ label: x.tag });
 			});
 
 			// Create quickPick
@@ -188,7 +194,7 @@ export async function activate(context: vscode.ExtensionContext) {
 			quickPick.ignoreFocusOut = true;
 
 			// QuickPick event
-			quickPick.onDidAccept(() =>{
+			quickPick.onDidAccept(() => {
 
 				// Hide quickPick
 				quickPick.hide();
@@ -199,7 +205,7 @@ export async function activate(context: vscode.ExtensionContext) {
 				// Modify active json document
 				editor.edit(editBuilder => {
 					// Delete all data
-					editBuilder.delete(new vscode.Range(new vscode.Position(0, 0), new vscode.Position(editor.document.lineCount-1, editor.document.lineAt(editor.document.lineCount-1).range.end.character)));
+					editBuilder.delete(new vscode.Range(new vscode.Position(0, 0), new vscode.Position(editor.document.lineCount - 1, editor.document.lineAt(editor.document.lineCount - 1).range.end.character)));
 
 					// Write new json data to file
 					editBuilder.insert(new vscode.Position(0, 0), JSON.stringify(data.parsedJSON, null, '\t'));
@@ -217,13 +223,13 @@ export async function activate(context: vscode.ExtensionContext) {
 				vscode.commands.executeCommand('json-keys-remover.removeKeys');
 			}
 		}
-		
+
 
 	});
 	context.subscriptions.push(disposable2);
 
-	
+
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
